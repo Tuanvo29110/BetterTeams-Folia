5bf602f2106bcf8d3a19369da66da2080d99f85e
Dungjava <99890979+maiminhdung@users.noreply.github.com>
2025-01-03 19:16:38 +0700

    Fix PlayerTeleport incomplete with some plugin

--- src/main/java/com/booksaw/betterTeams/PlayerTeleport.java
+++ src/main/java/com/booksaw/betterTeams/PlayerTeleport.java
@@ -3,6 +3,7 @@ package com.booksaw.betterTeams;
 import com.booksaw.betterTeams.message.MessageManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
@@ -13,86 +14,83 @@ import org.bukkit.event.player.PlayerTeleportEvent;
  */
 public class PlayerTeleport {
 
-	final String reference;
-	private final Player player;
-	private final Location location;
-	private final Location playerLoc;
+    final String reference;
+    private final Player player;
+    private final Location location;
+    private final Location playerLoc;
 
-	/**
-	 * This will start the cooldown (there is no delay)
-	 *
-	 * @param player    The player to teleport
-	 * @param location  the location to teleport them to
-	 * @param reference the reference for the message that should be sent when the
-	 *                  player is teleported
-	 * @throws Exception 
-	 */
-	public PlayerTeleport(Player player, Location location, String reference) throws Exception {
-		if (location == null || location.getWorld() == null) {
-			throw new Exception("Location or world is null");
-		}
-		
-		this.player = player;
-		this.location = location;
-		this.reference = reference;
+    /**
+     * This will start the cooldown (there is no delay)
+     *
+     * @param player    The player to teleport
+     * @param location  the location to teleport them to
+     * @param reference the reference for the message that should be sent when the
+     *                  player is teleported
+     * @throws Exception 
+     */
+    public PlayerTeleport(Player player, Location location, String reference) throws Exception {
+        if (location == null || location.getWorld() == null) {
+            throw new Exception("Location or world is null");
+        }
 
-		this.playerLoc = player.getLocation();
+        this.player = player;
+        this.location = location;
+        this.reference = reference;
 
-		
-		if (player.hasPermission("betterteams.warmup.bypass")) {
-			Main.plugin.getScheduler().runTask(this::runTp);
-			return;
-		}
+        this.playerLoc = player.getLocation();
 
-		int wait = Main.plugin.getConfig().getInt("tpDelay");
-		if (wait <= 0) {
-			Main.plugin.getScheduler().runTask(this::runTp);
-			return;
-		}
+        if (player.hasPermission("betterteams.warmup.bypass")) {
+            Main.plugin.getScheduler().runTask(() -> runTp(player, location));
+            return;
+        }
 
-		// sending the wait message
-		MessageManager.sendMessage(player, "teleport.wait", wait);
+        int wait = Main.plugin.getConfig().getInt("tpDelay");
+        if (wait <= 0) {
+            Main.plugin.getScheduler().runTask(() -> runTp(player, location));
+            return;
+        }
 
-		Main.plugin.getScheduler().runTaskLater(() -> {
-			if (canTp()) {
-				try {
-					runTp();
-				} catch (Exception e) {
-					throw new NullPointerException();
-				}
-			} else {
-				cancel();
-			}
-		}, 20L * wait);
+        // sending the wait message
+        MessageManager.sendMessage(player, "teleport.wait", wait);
 
-	}
+        Main.plugin.getScheduler().runTaskLater(() -> {
+            if (canTp()) {
+                try {
+                    runTp(player, location);
+                } catch (Exception e) {
+                    throw new NullPointerException();
+                }
+            } else {
+                cancel();
+            }
+        }, 20L * wait);
+    }
 
-	public void runTp() {
-		if (location == null || location.getWorld() == null) {
-			throw new NullPointerException("Location = " + location + " world is = " + ((location == null) ? location.getWorld() : "BLANK"));
-		}
+    public void runTp(Player player, Location location) {
+        try {
+            player.getScheduler().run(Main.plugin, scheduledTask -> {
+                World world = location.getWorld();
+                if (world != null) {
+                    world.getChunkAtAsync(location).thenAccept(chunk -> {
+                        player.teleportAsync(location, PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    });
+                }
+            }, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            MessageManager.sendMessage(player, "teleport.fail");
+        }
+    }
 
-		PlayerTeleportEvent event = new PlayerTeleportEvent(player, player.getLocation(), location);
-		Bukkit.getPluginManager().callEvent(event);
-		if (event.isCancelled()) {
-			return;
-		}
+    public boolean canTp() {
+        if (!Main.plugin.getConfig().getBoolean("noMove")) {
+            return true;
+        }
 
-		FoliaUtils.teleport(player, location);
-		MessageManager.sendMessage(player, reference);
-	}
+        return playerLoc.getWorld() == player.getLocation().getWorld() && playerLoc.distance(player.getLocation()) <= Math.abs(Main.plugin.getConfig().getInt("maxMove"));
+    }
 
-	public boolean canTp() {
-
-		if (!Main.plugin.getConfig().getBoolean("noMove")) {
-			return true;
-		}
-
-		return playerLoc.getWorld() == player.getLocation().getWorld() && playerLoc.distance(player.getLocation()) <= Math.abs(Main.plugin.getConfig().getInt("maxMove"));
-	}
-
-	public void cancel() {
-		MessageManager.sendMessage(player, "teleport.fail");
-	}
-
+    public void cancel() {
+        MessageManager.sendMessage(player, "teleport.fail");
+    }
 }
